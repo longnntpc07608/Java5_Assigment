560e90b5971523b2d9571a6db74b67c90a889815
longnntpc07608 <150583615+longnntpc07608@users.noreply.github.com>
2024-06-15 15:02:46 +0700

    commit

--- src/main/java/demo/controller/ProductsDetails.java
+++ src/main/java/demo/controller/ProductsDetails.java
@@ -19,6 +19,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,104 +33,83 @@ import demo.entity.Product;
 @RequestMapping("/admin")
 public class ProductsDetails {
 
-	@Autowired
-	ProductDAO daoProd;
-	
-	@Autowired
-	CategoryDAO daoCate;
-	
-	@ModelAttribute("availables")
-	public Map<Boolean, String> getAvailable(){
-		Map<Boolean, String> map = new HashMap<>();
-		map.put(true, " Available");
-		map.put(false, " Not Available");
-		return map;
-	}
-	
-	@ModelAttribute("categories")
-	public List<Category> getCategories(){
-		
-		List<Category> list = daoCate.findAll();
-				
-		return list;
-	}
+    @Autowired
+    ProductDAO daoProd;
+    
+    @Autowired
+    CategoryDAO daoCate;
+    
+    @ModelAttribute("availables")
+    public Map<Boolean, String> getAvailable(){
+        Map<Boolean, String> map = new HashMap<>();
+        map.put(true, " Available");
+        map.put(false, " Not Available");
+        return map;
+    }
+    
+    @ModelAttribute("categories")
+    public List<Category> getCategories(){
+        List<Category> list = daoCate.findAll();
+        return list;
+    }
 
-	@RequestMapping("/product")
-	public String product(
-			Model model,
-			@RequestParam("p") Optional<Integer> p,
-			@ModelAttribute("prod") Product product
-			) {
-		model.addAttribute("TitlePage", "Products Page");
-		model.addAttribute("views", "/views/admin/ProductsDetails.jsp");
+    @RequestMapping("/product")
+    public String product(
+            Model model,
+            @RequestParam("p") Optional<Integer> p,
+            @ModelAttribute("prod") Product product
+            ) {
+        model.addAttribute("TitlePage", "Products Page");
+        model.addAttribute("views", "/views/admin/ProductsDetails.jsp");
 
-		Sort sortCD = Sort.by(Direction.DESC,"createDate");
-		Pageable pageable = PageRequest.of(p.orElse(0), 5, sortCD);
-		Page<Product> page = daoProd.findAll(pageable);
-		model.addAttribute("page", page);
-		
-		return "/admin/index";
-	}
+        Sort sortCD = Sort.by(Direction.DESC,"createDate");
+        Pageable pageable = PageRequest.of(p.orElse(0), 5, sortCD);
+        Page<Product> page = daoProd.findAll(pageable);
+        model.addAttribute("page", page);
+        
+        return "/admin/index";
+    }
 
-	@RequestMapping("/product/edit/{id}")
-	public String edit(Model model, @PathVariable("id") Integer id, 
-			@RequestParam("p") Optional<Integer> p) {
-		model.addAttribute("TitlePage", "Products Page");
-		model.addAttribute("views", "/views/admin/ProductsDetails.jsp");
-		
-		Product product = daoProd.getById(id);
-		model.addAttribute("prod", product);
-		
-		
-		Sort sortCD = Sort.by(Direction.DESC,"createDate");
-		Pageable pageable = PageRequest.of(p.orElse(0), 5, sortCD);
-		Page<Product> page = daoProd.findAll(pageable);
-		model.addAttribute("page", page);
+    @RequestMapping("/product/edit/{id}")
+    public String edit(Model model, @PathVariable("id") Integer id, @ModelAttribute("prod") Product product) {
+        Optional<Product> opt = daoProd.findById(id);
+        if(opt.isPresent()) {
+            model.addAttribute("prod", opt.get());
+        } else {
+            model.addAttribute("prod", new Product());
+        }
+        return "forward:/admin/product";
+    }
 
-		return "/admin/index";
-	}
+    @RequestMapping("/product/delete/{id}")
+    public String delete(Model model, @PathVariable("id") Integer id) {
+        daoProd.deleteById(id);
+        model.addAttribute("message", "Delete successfully");
+        return "forward:/admin/product";
+    }
 
-	@RequestMapping(value = "/product/save", method = RequestMethod.POST)
-	public String save(
-			Model model,
-			@Validated @ModelAttribute("prod") Product item, 
-			BindingResult result) {
+    @PostMapping("/product/save")
+    public String saveProduct(
+            @Validated @ModelAttribute("prod") Product product,
+            BindingResult result,
+            Model model
+    ) {
+        if (result.hasErrors()) {
+            List<ObjectError> errors = result.getAllErrors();
+            for (ObjectError error : errors) {
+                System.out.println(error.getDefaultMessage());
+            }
+            model.addAttribute("views", "/views/admin/ProductsDetails.jsp");
+            return "/admin/index";
+        }
 
-		if(result.hasErrors()) {
-			List<ObjectError> errors = result.getAllErrors();
-			
-			System.out.println("message err: " + errors.get(0).getDefaultMessage());
-			
-			model.addAttribute("errors", errors);
+        daoProd.save(product);
+        return "redirect:/admin/product";
+    }
 
-			return "redirect:/admin/product";
-		}else {
-			if(item.getId() != null) {
-				daoProd.save(item);
-				
-				return "redirect:/admin/product/edit/" + item.getId();
-			}else {
-				daoProd.save(item);
-				return "redirect:/admin/product";
-			}
-						
-		}
-
-	}
-	
-	@RequestMapping(value = "/product/clear", method = RequestMethod.POST)
-	public String clear(Model model) {
-		Product product = new Product();
-		model.addAttribute("prod", product);
-		
-		return "redirect:/admin/product";
-	}
-
-	@RequestMapping("/product/delete/{id}")
-	public String delete(@PathVariable("id") Product product) {
-//		daoProd.deleteById(id);
-		daoProd.delete(product);
-		return "redirect:/admin/product";
-	}
-	
+    @RequestMapping("/product/clear")
+    public String clear(Model model, @ModelAttribute("prod") Product product) {
+        product = new Product();
+        return "forward:/admin/product";
+    }
 }
